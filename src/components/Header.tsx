0674ed94b700d5f9f96dc39ec01f90a12190930e
Joseph <joseph.clemente@hacktiv.io>
2024-09-14 18:06:07 +0800

    header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -22,8 +22,8 @@ export default function Header() {
         {/* <div className="absolute inset-0 bg-gradient-to-t from-black to-transparent z-10"></div> */}
         <div className="absolute inset-0 flex items-end sm:items-center justify-end text-right">
             <div className="px-9 py-10 sm:px-32 w-full sm:w-1/2">
-                <p className={`text-lg text-green-950 ${calligraphy.className}`}>Dearest Gentle Reader,</p>
-                <p className="text-4xl text-green-950">Join Us as We Celebrate Love, Laughter, and a Lifetime Together</p>
+                <p className={`text-lg font-semibold text-green-950`}>Dearest Gentle Reader,</p>
+                <p className="text-2xl font-bold sm:text-4xl text-green-950 text-modern-text">Join Us as We Celebrate Love, Laughter, and a Lifetime Together</p>
             </div>
         </div>
     </div>
