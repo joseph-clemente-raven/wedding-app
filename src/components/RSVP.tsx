2a40952b1445717fcf45771181a52b919e30c560
Joseph <joseph.clemente@hacktiv.io>
2024-10-19 20:00:05 +0800

    fixed

--- src/components/RSVP.tsx
+++ src/components/RSVP.tsx
@@ -3,10 +3,10 @@ import React from 'react'
 export default function RSVP() {
   return (
     <div className='flex items-center min-h-80 flex-col p-20 gap-4 justify-center'>
-        <h1 className='font-bold text-4xl text-center'>RSVP</h1>
-        <p>Join Us for an Unforgettable Experience!</p>
-        <p>We're excited to celebrate together! Please let us know if you can make it by filling out the RSVP form below. Your presence will make our event even more special. Thank you, and we look forward to seeing you soon!</p>
-        <p>Click here to <a target='_blank' href='https://forms.gle/5gpwr7rgj56MY37D7' className='font-bold'>RSVP</a></p>
+      <h1 className='font-bold text-4xl text-center'>RSVP</h1>
+      <p>Join Us for an Unforgettable Experience!</p>
+      <p>We&apos;re excited to celebrate together! Please let us know if you can make it by filling out the RSVP form below. Your presence will make our event even more special. Thank you, and we look forward to seeing you soon!</p>
+      <p>Click here to <a target='_blank' href='https://forms.gle/5gpwr7rgj56MY37D7' className='font-bold'>RSVP</a></p>
     </div>
   )
 }
