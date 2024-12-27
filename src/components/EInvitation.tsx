74bbf495efeb7e9d88fb9d46fbc970278b5a8719
Joseph <joseph.clemente@hacktiv.io>
2024-12-27 10:17:37 +0800

    downloadable invitation

--- src/components/EInvitation.tsx
+++ src/components/EInvitation.tsx
@@ -1,29 +1,42 @@
+
+'use client';
+
 import React from 'react';
 import Image from 'next/image';
 import { Dancing_Script } from 'next/font/google';
-import { I1, I2, I3, I4 } from '../../public/invitation';
 
 const dancing_script = Dancing_Script({ weight: '700', preload: false });
 
 export default function EInvitation() {
   const images = [
-    I1,
-    I2,
-    I3,
-    I4
-  ]; // Replace with actual image paths in the public folder
+    './invitation/1.png',
+    './invitation/2.png',
+    './invitation/3.png',
+    './invitation/4.png',
+  ]; // Path to images in public folder
+
+  // Function to download each image as a separate file
+  const downloadImages = () => {
+    images.forEach((image, index) => {
+      // Create an anchor tag programmatically
+      const link = document.createElement('a');
+      link.href = image;
+      link.download = `invitation_${index + 1}.jpg`; // Specify the download file name
+
+      // Trigger the download
+      link.click();
+    });
+  };
 
   return (
     <div className="flex flex-col items-center min-h-screen p-4 sm:p-6 gap-4 justify-center my-12 sm:my-0">
       <h1 className={`font-bold text-4xl text-center ${dancing_script.className}`}>
         E-Invitation
       </h1>
+      <p onClick={downloadImages} className="font-bold cursor-pointer hover:text-green-500">(Download Invitation)</p>
       <div className="w-full flex flex-col gap-10 px-0 sm:px-60">
         {images.map((image, index) => (
-          <div
-            key={index}
-            className="snap-center flex justify-center items-center"
-          >
+          <div key={index} className="snap-center flex justify-center items-center">
             <Image
               src={image}
               alt={`Slide ${index + 1}`}
