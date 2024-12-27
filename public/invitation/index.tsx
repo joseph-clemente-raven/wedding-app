1516f6e137bdf8b54fb4920ed4fe355b9166e069
Joseph <joseph.clemente@hacktiv.io>
2024-12-27 09:05:30 +0800

    e-invitation

---
+++ public/invitation/index.tsx
@@ -0,0 +1,11 @@
+import I1 from './1.png';
+import I2 from './2.png';
+import I3 from './3.png';
+import I4 from './4.png';
+
+export {
+    I1,
+    I2,
+    I3,
+    I4,
+}
--- src/components/EInvitation.tsx
+++ src/components/EInvitation.tsx
@@ -1,13 +1,40 @@
-import React from 'react'
-
+import React from 'react';
+import Image from 'next/image';
 import { Dancing_Script } from 'next/font/google';
+import { I1, I2, I3, I4 } from '../../public/invitation';
 
 const dancing_script = Dancing_Script({ weight: '700', preload: false });
 
 export default function EInvitation() {
+  const images = [
+    I1,
+    I2,
+    I3,
+    I4
+  ]; // Replace with actual image paths in the public folder
+
   return (
-    <div className='flex items-center min-h-80 flex-col p-0 sm:p-6 sm:pb-20 gap-4 justify-center'>
-        <h1 className={`font-bold text-4xl text-center ${dancing_script.className}`}>E-Invitation</h1>
+    <div className="flex flex-col items-center min-h-screen p-4 sm:p-6 gap-4 justify-center my-12 sm:my-0">
+      <h1 className={`font-bold text-4xl text-center ${dancing_script.className}`}>
+        E-Invitation
+      </h1>
+      <div className="w-full flex flex-col gap-10 px-0 sm:px-60">
+        {images.map((image, index) => (
+          <div
+            key={index}
+            className="snap-center flex justify-center items-center"
+          >
+            <Image
+              src={image}
+              alt={`Slide ${index + 1}`}
+              width={1000} // Adjust dimensions to match your layout
+              height={1000}
+              className="max-h-full max-w-full object-contain"
+              priority={index === 0} // Optimize the first image
+            />
+          </div>
+        ))}
+      </div>
     </div>
-  )
+  );
 }
